2bbf905176ccb7034b19783ca3421ec5eedae606
Jake Tamayo <hellojtmatthew@icloud.com>
2022-05-18 16:57:19 -0500

    Initial Commit

---
+++ Astronauts/AstronautsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronautsApp.swift
+//  Astronauts
+//
+//  Created by Jacob Tamayo on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct AstronautsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
